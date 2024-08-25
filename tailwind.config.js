64cf11eeb0f89ec626f47cde5dc99f26a9325795
prince galib <galib01prince@gmail.com>
2024-08-25 23:47:54 +0600

    4th update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,12 @@ module.exports = {
     },
     plugins: [],
   }
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('assets/bg.jpg')",
+        }
+      }
+    }
+  }
